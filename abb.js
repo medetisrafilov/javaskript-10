9bdb61a42d8c992f374ee0a42569e706bf3032f3
medetisrafilov <israfilovmedet52@gmail.com>
2024-12-05 16:19:36 -1200

    homework

---
+++ abb.js
@@ -0,0 +1,18 @@
+const massiv = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ];
+
+
+
+
+const birinci = massiv [0];
+const ikinci = massiv[1];
+
+const yeniMassiv = massiv.slice(2);
+
+
+console.log("birinci eded:", birinci);
+console.log("ikinci eded:", ikinci);
+console.log("yeni massiv:", yeniMassiv);
+
+
+
+
